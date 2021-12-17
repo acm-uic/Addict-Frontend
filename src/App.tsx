b1620b26de0365462c07e790732f765c7558eeb5
christianbingman <christian@christianbingman.com>
2021-12-16 19:44:50 -0600

    Added basic API call for the token, displaying it on the screen when received

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
+import API from './util/Api'
 
 function App() {
+  // Create the api object and store its state
+  const [api, setAPI] = useState(new API(""));
+  const [username, updateUsername] = useState("");
+  const [password, updatePassword] = useState("");
+  
   return (
     <div className="App container-lg">
       <h1>ACM@UIC IAM</h1>
+      <div className="form-group">
+        <input onChange={event => updateUsername(event.target.value)} type="text" name="username" className='form-control my-2' />
+        <input onChange={event => updatePassword(event.target.value)} type="password" name="password" className='form-control my-2' />
+        {/*When updating the actual API object, you need to add the variable to the constructor and create a new object! THIS MEANS DON'T ADD EVERYTHING TO THE API THAT ISN'T FUNCTIONS*/}
+        <button className='btn btn-primary my-2' onClick={() => api.getTokenFromAPI(username, password).then(res => setAPI(new API(res.token)))}>Log In</button>
+      </div>
+      {/* Print the API token out for debugging; BTW this is how you define a comment in TSX*/}
+      <h1>{api.token}</h1>
     </div>
   );
 }
