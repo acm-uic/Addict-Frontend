f17559afda8288e7f4b801b4073b660703615c2e
Adrian Knight <aknig2@uic.edu>
2022-02-24 13:29:53 -0600

    CreateUser page creates user
    Create user page can now take user information and create a user

--- src/pages/Create.tsx
+++ src/pages/Create.tsx
@@ -1,30 +1,37 @@
 
+import { useSelector } from "react-redux";
+import { apiReducerState } from "../redux/reducers/apikey";
+import API, { User } from "../util/Api";
 export default function Create(): JSX.Element {
-    
-    function Form() {
-        
-        return 
+    let firstName: string = ""
+    let lastName: string = ""
+    let username: string = ""
+    let email: string = ""
+    let description: string = ""
+
+    const server = useSelector((state: apiReducerState) => state.server);
+    const apikey = useSelector((state: apiReducerState) => state.key);
+
+    async function CreateUser() {
+        alert(await API.createUser(firstName, lastName, username, email, description, apikey, server));
     }
-    
     return (<div>
         <h1>Create a user</h1>
-        <form>
-                <label><h2>First name:</h2></label>
-                <input type="text" name="firstName"/><br/>
-                <label><h2>Last name:<br /></h2></label>
-                <input type="text" name="firstName"/><br/>
-                <label><h2>Username:<br /></h2></label>
-                <input type="text" name="firstName"/><br/>
-                <label><h2>Email:<br /></h2></label>
-                <input type="text" name="firstName"/><br/>
-                <label><h2>Password:<br /></h2></label>
-                <input type="text" name="firstName"/>
-                
-            </form>
-        
-        
-        
 
+        <label><h2>First name:</h2></label>
+        <input type="text" onChange={event => firstName = event.target.value}/><br/>
+        <label><h2>Last name:<br /></h2></label>
+        <input type="text" onChange={event => lastName = event.target.value}/><br/>
+        <label><h2>Username:<br /></h2></label>
+        <input type="text" onChange={event => username = event.target.value}/><br/>
+        <label><h2>Email:<br /></h2></label>
+        <input type="text" onChange={event => email = event.target.value}/><br/>
+        <label><h2>Description:<br /></h2></label>
+        <input type="text" onChange={event => description = event.target.value}/><br/>
+
+        <input type="button" value="Submit" onClick={CreateUser}/>
+
+        
         </div>)
 
     
