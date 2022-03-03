6e326c30831c179c16eb66526fe163ebad298d17
christianbingman <christian@christianbingman.com>
2022-03-03 13:33:03 -0600

    WIP

--- src/components/UserEditView.tsx
+++ src/components/UserEditView.tsx
@@ -6,7 +6,14 @@ interface props {
 }
 
 export default function UserEditView(props: props): JSX.Element {
-    return (<div id={"user-card-edit-" + props.user.cn} className={"container-sm user-card-edit"}>Test
+    return (<div id={"user-card-edit-" + props.user.cn} className={"container-sm user-card-edit"}>
+        <div className="inputs">
+            <input type="text" placeholder='First Name' defaultValue={props.user.cn} />
+            <input type="text" placeholder='Description' defaultValue={props.user.description} />
+            <input type="text" placeholder='Distinguished Name' disabled={true} defaultValue={props.user.dn} />
+            <input type="text" placeholder='sAMAccountName' defaultValue={props.user.sAMAccountName} />
+        </div>
+        
     <div id={"drop-arrow-" + props.user.cn} className={"drop-arrow"} onClick={() => {
         document.getElementById("drop-arrow-" + props.user.cn)!.classList.toggle("drop-arrow--animating");
         document.getElementById('user-card-edit-' + props.user.cn)!.classList.toggle("user-card-edit--animating");
