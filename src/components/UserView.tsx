02e8fb83d54216ba7c922405dbc5016c513120e3
Adrian Knight <aknig2@uic.edu>
2022-03-31 21:38:06 -0500

    userPage: password set and expire date
    The date will now display however, adding a time library like moment.js from npm is a good idea. The expire date seems to be off by a day

--- src/components/UserView.tsx
+++ src/components/UserView.tsx
@@ -1,3 +1,4 @@
+import { isExportDeclaration } from 'typescript';
 import { User } from '../util/Api'
 import './UserView.scss'
 
@@ -5,6 +6,11 @@ interface props {
     user: User
 }
 export default function UserView(props: props): JSX.Element {
+    let n = parseInt(props.user.pwdLastSet);
+    let date = new Date(n/1e4 - 1.16444736e13);
+    let dateString = date.toUTCString();
+    let expiration = new Date(date.setFullYear(date.getFullYear() + 1))
+    let expirationString = expiration.toUTCString()
 
     return (<div className="user-card container-sm">
         <div className="side">
@@ -13,7 +19,8 @@ export default function UserView(props: props): JSX.Element {
         </div>
         <div className="mid">
             <div className="dn">{props.user.dn}</div>
-            <div className="pw">Password Last Set: {props.user.pwdLastSet}</div>
+            <div className="pw">Password Last Set: {dateString}</div>
+            <div className="pw">Expire date: {expirationString}</div>
             <div className="groups">Groups: {props.user.groups.map(group => group.cn).join(',')}</div>
             <div className="created">Created: {props.user.whenCreated}</div>
             <div className="description">{props.user.description}</div>
