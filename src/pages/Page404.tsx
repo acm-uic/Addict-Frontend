38d7e8120541f96a03dd9f09ce61cd6b185da451
christianbingman <christian@christianbingman.com>
2022-05-13 17:25:40 -0500

    Updated UI Elements
    Updated Global CSS
    - Added input field visuals
    - Added primary and secondary button visuals
    - Added color variables

--- src/pages/Page404.tsx
+++ src/pages/Page404.tsx
@@ -2,7 +2,10 @@ export default function Page404(): JSX.Element {
     // Return a medium bootstrap container with a heading of "Unauthenticated"
     return (
         <div className="container-sm">
-                    <h1>Page Not Found!</h1>
+                    <h1>Testing UI</h1>
+                    <input type="text" placeholder="Username:"/>
+                    <div className="button">Login</div>
+                    <div className="button-secondary">Add User</div>
         </div>
     );
 }
