a51011877b29840038a4bab2b44d0a3424618a4f
christianbingman <christian@christianbingman.com>
2022-05-13 16:54:55 -0500

    Cleaned up pages and added 404

---
+++ src/pages/Page404.tsx
@@ -0,0 +1,8 @@
+export default function Page404(): JSX.Element {
+    // Return a medium bootstrap container with a heading of "Unauthenticated"
+    return (
+        <div className="container-sm">
+                    <h1>Page Not Found!</h1>
+        </div>
+    );
+}
