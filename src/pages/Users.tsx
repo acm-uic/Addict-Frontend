168a36231b843e048e292567e7d7a1035c7d7bea
christianbingman <christian@christianbingman.com>
2022-06-02 18:29:02 -0500

    Added Users Table
    Added the users table, but it is still missing a few features
    - Needs an edit button
    - Needs the enabled switch

--- src/pages/Users.tsx
+++ src/pages/Users.tsx
@@ -4,7 +4,7 @@ import UserEditView from "../components/UserEditView";
 import UserView from "../components/UserView";
 import { Link } from "react-router-dom";
 import { apiReducerState } from "../redux/reducers/apikey";
-import API, { User } from "../util/Api";
+import API, { User, Group } from "../util/Api";
 import './Users.scss'
 export default function Users(): JSX.Element {
     const server = useSelector((state: apiReducerState) => state.server);
@@ -15,8 +15,19 @@ export default function Users(): JSX.Element {
     // Load only once
     useEffect(() => {
         API.getAllUsers(server, apikey).then(users => setUsers(users)).catch(err => console.log(err))
+        setUsers([{cn: "Christian Bingman", dn: "CN=Christian Bingman,OU=Users,DC=acmuic,DC=org", whenCreated: "0", pwdLastSet: "0", description: "Test Description", sAMAccountName: "cbingm2", userAccountControl: "0", groups: []}])
     }, [])
     
+    function getTableRow(name: String, email: String, groups: Group[], username: String): JSX.Element{
+        return <tr>
+            <td>{name}</td>
+            <td>{email}</td>
+            <td>{groups.join('\n')}</td>
+            <td>{username}</td>
+            <td></td>
+            <td></td>
+        </tr>
+    }
 
     function getSearchBar(): JSX.Element {
         return (<div className="container-lg">
@@ -37,8 +48,13 @@ export default function Users(): JSX.Element {
                 <th scope="col">EDIT</th>
                 <th scope="col">ACTIVE</th></tr>
             </thead>
-        </table>
+            <tbody>
             {users.filter(
-                (user: User) => {})}
+                    (user: User) => user.cn.includes(searchQuery) || user.sAMAccountName.includes(searchQuery) || user.description.includes(searchQuery)).map(
+                        (user: User) => getTableRow(user.cn, user.sAMAccountName + "@uic.edu", user.groups, user.sAMAccountName)
+                    )}
+                
+            </tbody>
+        </table>
         </div>)
 }
