48921d5b5843bb9880fd5562022f07964ccea252
dhelle2 <dhelle2@uic.edu>
2022-06-02 20:32:09 -0500

    login

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,41 @@
-import { useSelector } from "react-redux"
+import { useDispatch,useSelector } from "react-redux"
 import { apiReducerState } from "../redux/reducers/apikey"
+import { useNavigate } from "react-router-dom";
+
+import API from "../util/Api"
+
+import people from '../img/people.jpg'
+import logo from '../img/logo.png'
+import "./Home.scss"
 
 export default function Home(): JSX.Element {
     const apikey = useSelector((state: apiReducerState) => state.key)
-    return (<div><h1>Home</h1><br /> <h3>{apikey}</h3></div>)
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
+    const server: string = useSelector((state: apiReducerState) => state.server);
+    let username = "";
+    let password = "";
+    let token: string = useSelector((state: apiReducerState) => state.key);
+    const loggedIn = useSelector((state: apiReducerState) => state.loggedIn)
+    const loggedInUser = useSelector((state: apiReducerState) => state.user)
+
+    async function HandleSubmit(): Promise<void> {
+        token = await API.getTokenFromAPI(username, password, server)
+        dispatch({type: "UPDATE_USER", payload: (await API.getUser(username, token, server)).cn})
+        dispatch({type: "UPDATE_KEY", payload: token})
+        navigate('/authorized')
+    }
+
+    return (
+        <div className='back'>
+            <img id='acm' src={people}></img>
+            <div id='box'>
+                <img src = {logo}></img>
+                <input id='username' type='text' placeholder='Username' onChange={event => username = event.target.value}></input>
+                <input id='password' type='password' placeholder="Password" onChange={event => password = event.target.value}></input>
+                <div className='button' onClick={() => {HandleSubmit()} }>Login</div>
+            </div>
+        </div>
+    )
+    
 }
