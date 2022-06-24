5716cce26d07c5bfa99c69f0df9f487dd46ebd30
GabrielGustafsonUIC <60358481+GabeGustafson@users.noreply.github.com>
2022-06-23 20:47:17 -0500

    Create User page figma model

--- src/pages/Create.tsx
+++ src/pages/Create.tsx
@@ -1,42 +1,51 @@
-
+import { useState } from "react";
 import { useSelector } from "react-redux";
 import { apiReducerState } from "../redux/reducers/apikey";
 import API from "../util/Api";
+import './Create.scss'
+
+const UIC_DOMAIN = 'uic.edu';
+
+const InputBar = ({placeholder, inputText, setText} 
+    : {placeholder: string, inputText: string, setText: React.Dispatch<React.SetStateAction<string>>}) => {
+    return (
+        <input type="text" 
+            value={inputText}
+            onChange={event => setText(event.target.value)}
+            placeholder={placeholder}
+            className="input-bar"/>
+        );
+}
+
 export default function Create(): JSX.Element {
-    let firstName: string = ""
-    let lastName: string = ""
-    let username: string = ""
-    let email: string = ""
-    let description: string = ""
+    const [username, setUsername] = useState("");
+    const [firstName, setFirstName] = useState("");
+    const [lastName, setLastName] = useState("");
+    const [password, setPassword] = useState("");
+    const [verify, setVerify] = useState("");
+    const [description, setDescription] = useState("");
 
     const server = useSelector((state: apiReducerState) => state.server);
     const apikey = useSelector((state: apiReducerState) => state.key);
 
     async function CreateUser() {
-        alert(await API.createUser(firstName, lastName, username, email, description, apikey, server));
+        alert(await API.createUser(firstName, lastName, username, `${username}@${UIC_DOMAIN}`, description, apikey, server));
     }
-    return (<div>
-        <h1>Create a user</h1>
 
-        <label><h2>First name:</h2></label>
-        <input type="text" onChange={event => firstName = event.target.value}/><br/>
+    return (<div className="create-user-box">
+        <h1 className="create-user-header">Create User</h1>
 
-        <label><h2>Last name:<br /></h2></label>
-        <input type="text" onChange={event => lastName = event.target.value}/><br/>
+        <InputBar placeholder="Login Name" inputText={username} setText={setUsername}/>
+        <InputBar placeholder="First Name" inputText={firstName} setText={setFirstName}/>
+        <InputBar placeholder="Last Name" inputText={lastName} setText={setLastName}/>
+        <InputBar placeholder="Password (Optional)" inputText={password} setText={setPassword}/>
+        <InputBar placeholder="Verify Password (Optional)" inputText={verify} setText={setVerify}/>
+        <InputBar placeholder="Description" inputText={description} setText={setDescription}/>
 
-        <label><h2>Username:<br /></h2></label>
-        <input type="text" onChange={event => username = event.target.value}/><br/>
-
-        <label><h2>Email:<br /></h2></label>
-        <input type="text" onChange={event => email = event.target.value}/><br/>
-        
-        <label><h2>Description:<br /></h2></label>
-        <input type="text" onChange={event => description = event.target.value}/><br/>
-
-        <input type="button" value="Submit" onClick={CreateUser}/>
+        <input type="button" value="Create User" onClick={CreateUser} className="create-user-button"/>
 
         
-        </div>)
+        </div>);
 
     
 }
