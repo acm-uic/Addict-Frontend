61426d2b17c343f785b5454b92a63a1a72805ec8
Adrian Knight <aknig2@uic.edu>
2022-06-23 20:58:29 -0500

    ChangePassword should take passwords

--- src/pages/ChangePassword.tsx
+++ src/pages/ChangePassword.tsx
@@ -13,29 +13,35 @@ export default function ChangePassword(): JSX.Element {
 
     const server = useSelector((state: apiReducerState) => state.server);
     const [searchParams, setSearchParams] = useSearchParams();
-    
+
     async function ChangePass() {
+        // Check newPass and conPass to be the same and alert if not
         const apikey = searchParams.get("token")
         console.log(apikey)
-        if (apikey) {
-            alert(await API.changePassword(username,newPass,apikey,server));
+        if (newPass === conPass) {
+            if (apikey) {
+                alert(await API.changePassword(username, newPass, apikey, server));
+            }
+        } else {
+            alert("Passwords must match");
         }
+
     }
     return (
         <div id="change-password">
-            
-            <div id = "login">
-                <div id = "img">
-                <img id="logo" src={logo}/><br/>
+
+            <div id="login">
+                <div id="img">
+                    <img id="logo" src={logo} /><br />
                 </div>
                 <h1>Change password</h1>
                 {/* <label><h2>Username:</h2></label> */}
-                <input type="text" placeholder={"New Password"} onChange = {event => newPass = event.target.value}/><br/>
+                <input type="text" placeholder={"New Password"} onChange={event => newPass = event.target.value} /><br />
                 {/* <label><h2>New password:</h2></label> */}
-                <input type="password" placeholder={"Confirm Password"} onChange = {event => conPass = event.target.value}/><br/>
+                <input type="password" placeholder={"Confirm Password"} onChange={event => conPass = event.target.value} /><br />
                 <div className="button" onClick={ChangePass}>Submit</div>
             </div>
         </div>
-        
+
     )
 }
