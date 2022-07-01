371a7a85c2d23e2e94009cca63a8816fbffae583
GabrielGustafsonUIC <60358481+GabeGustafson@users.noreply.github.com>
2022-06-30 20:00:12 -0500

    Create User Page with normal inputs

--- src/pages/Create.tsx
+++ src/pages/Create.tsx
@@ -6,24 +6,22 @@ import './Create.scss'
 
 const UIC_DOMAIN = 'uic.edu';
 
-const InputBar = ({placeholder, inputText, setText} 
-    : {placeholder: string, inputText: string, setText: React.Dispatch<React.SetStateAction<string>>}) => {
+function getInputBar(placeholder: string, setField: Function): JSX.Element {
     return (
         <input type="text" 
-            value={inputText}
-            onChange={event => setText(event.target.value)}
+            onChange={event => setField(event.target.value)}
             placeholder={placeholder}
             className="input-bar"/>
         );
 }
 
 export default function Create(): JSX.Element {
-    const [username, setUsername] = useState("");
-    const [firstName, setFirstName] = useState("");
-    const [lastName, setLastName] = useState("");
-    const [password, setPassword] = useState("");
-    const [verify, setVerify] = useState("");
-    const [description, setDescription] = useState("");
+    let username = "";
+    let firstName = "";
+    let lastName = "";
+    let password = "";
+    let verify = "";
+    let description = "";
 
     const server = useSelector((state: apiReducerState) => state.server);
     const apikey = useSelector((state: apiReducerState) => state.key);
@@ -35,15 +33,14 @@ export default function Create(): JSX.Element {
     return (<div className="create-user-box">
         <h1 className="create-user-header">Create User</h1>
 
-        <InputBar placeholder="Login Name" inputText={username} setText={setUsername}/>
-        <InputBar placeholder="First Name" inputText={firstName} setText={setFirstName}/>
-        <InputBar placeholder="Last Name" inputText={lastName} setText={setLastName}/>
-        <InputBar placeholder="Password (Optional)" inputText={password} setText={setPassword}/>
-        <InputBar placeholder="Verify Password (Optional)" inputText={verify} setText={setVerify}/>
-        <InputBar placeholder="Description" inputText={description} setText={setDescription}/>
+        {getInputBar("Login Name", (val: string) => {username=val})}
+        {getInputBar("First Name", (val: string) => {firstName=val})}
+        {getInputBar("Last Name", (val: string) => {lastName=val})}
+        {getInputBar("Password (Optional)", (val: string) => {password=val})}
+        {getInputBar("Verify Password (Optional)", (val: string) => {verify=val})}
+        {getInputBar("Description", (val: string) => {description=val})}
 
-        <input type="button" value="Create User" onClick={CreateUser} className="create-user-button"/>
-
+        <div onClick={CreateUser} className="button-secondary create-user-button">Create User</div>
         
         </div>);
 
