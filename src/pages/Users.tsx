2b7cca70b3518e75cd651c913b0002243300ea43
christianbingman <christian@christianbingman.com>
2022-07-21 19:44:27 -0500

    Started dialog box
    There are some issues with bootstrap and react/typescript conflicting that are making the modal not show up

--- src/pages/Users.tsx
+++ src/pages/Users.tsx
@@ -15,7 +15,9 @@ export default function Users(): JSX.Element {
 
     // Load only once
     useEffect(() => {
-        API.getAllUsers(server, apikey).then(users => setUsers(users)).catch(err => console.log(err))
+        //API.getAllUsers(server, apikey).then(users => setUsers(users)).catch(err => console.log(err))
+        setUsers([{sAMAccountName: "tuser2", dn: "Test", whenCreated: "0", pwdLastSet: "0", userAccountControl: "0", cn: "Test User", description: "Description", groups: []}])
+        
     }, [])
     
     function getTableRow(name: String, email: String, groups: Group[], username: String): JSX.Element{
@@ -25,7 +27,7 @@ export default function Users(): JSX.Element {
             <td>{groups.join('\n')}</td>
             <td>{username}</td>
 
-            <td><div className="edit-button"><img className="pensvg" src={pensolid} alt="Edit Pen" /></div></td>
+            <td><button className="edit-button" data-bs-toggle="modal" data-bs-target="#exampleModal"><img className="pensvg" src={pensolid} alt="Edit Pen" /></button></td>
             <td><div className="enable-switch"></div></td>
 
         </tr>
@@ -43,10 +45,27 @@ export default function Users(): JSX.Element {
                     <Link to="/admin/create" className="nav-item nav-link add-user"><p>Add User</p></Link>
                 </div>)
     }
-    
+
 
     return (<div className="container-lg">
         {getSearchBar()}
+        <div className="modal fade" id="exampleModal" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div className="modal-dialog">
+    <div className="modal-content">
+      <div className="modal-header">
+        <h5 className="modal-title" id="exampleModalLabel">Modal title</h5>
+        <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div className="modal-body">
+        ...
+      </div>
+      <div className="modal-footer">
+        <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+        <button type="button" className="btn btn-primary">Save changes</button>
+      </div>
+    </div>
+  </div>
+</div>
         <table className="table">
             <thead>
                 <tr><th scope="col">NAME</th>
