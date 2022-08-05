fb8a5cf3f1485c1740b52c6fa82d57546846fc9e
christianbingman <christian@christianbingman.com>
2022-08-04 21:05:04 -0500

    Added edit user modal

--- src/pages/Users.tsx
+++ src/pages/Users.tsx
@@ -1,11 +1,11 @@
 import { useEffect, useState } from "react";
-import { useSelector } from "react-redux";
 
-import { Link } from "react-router-dom";
+import { useSelector } from "react-redux";
 import { apiReducerState } from "../redux/reducers/apikey";
-import API, { User, Group } from "../util/Api";
+import API from "../util/Api"
+import { Link } from "react-router-dom";
+import { User } from "../util/Api";
 import pensolid from "../svg/pen-solid.svg"
-import Button from 'react-bootstrap/Button';
 import Modal from 'react-bootstrap/Modal';
 
 import './Users.scss'
@@ -15,22 +15,22 @@ export default function Users(): JSX.Element {
     let [searchQuery, setSearchQuery] = useState("");
     let [users, setUsers] = useState<User[]>([]);
     const [show, setShow] = useState(false);
+    let [editingUser, setEditingUser] = useState<User>({sAMAccountName: "", dn: "", groups: [], whenCreated: "", pwdLastSet: "", cn: "", description: "", userAccountControl: ""})
 
     // Load only once
     useEffect(() => {
-        //API.getAllUsers(server, apikey).then(users => setUsers(users)).catch(err => console.log(err))
-        setUsers([{sAMAccountName: "tuser2", dn: "Test", whenCreated: "0", pwdLastSet: "0", userAccountControl: "0", cn: "Test User", description: "Description", groups: []}])
+        API.getAllUsers(server, apikey).then(users => setUsers(users)).catch(err => console.log(err))
         
     }, [])
     
-    function getTableRow(name: String, email: String, groups: Group[], username: String): JSX.Element{
-        return <tr>
-            <td>{name}</td>
-            <td>{email}</td>
-            <td>{groups.join('\n')}</td>
-            <td>{username}</td>
+    function getTableRow(user: User): JSX.Element{
+        return <tr key={user.sAMAccountName as string}>
+            <td>{user.cn}</td>
+            <td>{user.sAMAccountName + "@uic.edu"}</td>
+            <td>{user.groups.join('\n')}</td>
+            <td>{user.sAMAccountName}</td>
 
-            <td><button className="edit-button" onClick={() => setShow(true)}><img className="pensvg" src={pensolid} alt="Edit Pen" /></button></td>
+            <td><div className="edit-button" onClick={() => handleOpen(user)}><img className="pensvg" src={pensolid} alt="Edit Pen" /></div></td>
             <td><div className="enable-switch"></div></td>
 
         </tr>
@@ -48,23 +48,28 @@ export default function Users(): JSX.Element {
                     <Link to="/admin/create" className="nav-item nav-link add-user"><p>Add User</p></Link>
                 </div>)
     }
-    const handleClose = () => setShow(false)
+
+    const handleOpen = (user: User) => {
+        setEditingUser(user);
+        setShow(true)
+    }
+    const handleClose = () => {setShow(false)}
 
     function showModal(): JSX.Element {
         return (
-        <Modal show={show} onHide={handleClose}>
-        <Modal.Header closeButton>
-          <Modal.Title>Modal heading</Modal.Title>
-        </Modal.Header>
-        <Modal.Body>Woohoo, you're reading this text in a modal!</Modal.Body>
-        <Modal.Footer>
-          <Button variant="secondary" onClick={handleClose}>
-            Close
-          </Button>
-          <Button variant="primary" onClick={handleClose}>
-            Save Changes
-          </Button>
-        </Modal.Footer>
+        <Modal  show={show} onHide={handleClose} centered>
+        <Modal.Body>
+            <h1>{editingUser.cn}</h1>
+            <input type="text" placeholder="First Name" />
+            <input type="text" placeholder="Last Name" />
+            <input type="text" placeholder={editingUser.sAMAccountName} />
+            <input type="text" placeholder="Mail" />
+            <input type="text" placeholder="New Password" />
+            <input type="text" placeholder="New Password (Verify)" />
+            <input type="text" placeholder="Gropus to Add (Comma Seperated)" />
+            <input type="text" placeholder="Gropus to Remove (Comma Seperated)" />
+            <div className="button-secondary modal-button">Update User</div>
+        </Modal.Body>
       </Modal>
                )
     }
@@ -84,7 +89,7 @@ export default function Users(): JSX.Element {
             <tbody>
             {users.filter(
                     (user: User) => user.cn.includes(searchQuery) || user.sAMAccountName.includes(searchQuery) || user.description.includes(searchQuery)).map(
-                        (user: User) => getTableRow(user.cn, user.sAMAccountName + "@uic.edu", user.groups, user.sAMAccountName)
+                        (user: User) => getTableRow(user)
                     )}
                 
             </tbody>
